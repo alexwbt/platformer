e96448c6f6628170c10cd10fe1fcb84149ca5c99
alex.wong <alex.wong@easyview.com.hk>
2022-07-06 09:34:54 +0800

    update

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.use(require('cors')());
+app.use(require('cors')("*"));
 app.use(express.static('game'));
 
 // game
